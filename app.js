38e9b0f6d3ae8f97185bacb5938df2a4153e15d4
Humberto Teruel <humberto23teruel@gmail.com>
2022-07-02 21:44:38 -0300

    generar una funcion que devuelva un item aleatorio para la computadora

--- app.js
+++ app.js
@@ -1,28 +1,6 @@
-const choises = ['Rock', 'Paper', 'Scissors'];
-/*const playerSelection = prompt("Choose your weapon");*/
-let playerSelection = "Paper";
-let computerSelection = "Paper";
-
+const choises = ["rock", "paper", "scissors"];
 
 function computerPlay() {
-    let computerChoise = choises[Math.floor(Math.random() * 3)];
+    computerChoise = choises[Math.floor(Math.random() * 3)];
     return computerChoise;
 }
-
-function playRound (playerSelection, computerSelection) {
-    if( playerSelection === "Rock" && computerSelection === "Paper" ||
-        playerSelection === "Paper" && computerSelection === "Scissors" ||
-        playerSelection === "Scissors" && computerSelection === "Rock") {
-            return "You Lose!!!"
-        }
-    else if( playerSelection === "Paper" && computerSelection === "Rock" ||
-    playerSelection === "Scissors" && computerSelection === "Paper" ||
-    playerSelection === "Rock" && computerSelection === "Scissors") {
-        return "You Win!!!"
-    }
-    else {
-        return "It's a Tie";
-    }
-}
-
-
