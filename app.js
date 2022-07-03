7e2b2549f449366023dae417b171fe002ee1dcc5
Humberto Teruel <humberto23teruel@gmail.com>
2022-07-02 22:42:39 -0300

    Crear funcion que devuelva el resultado de un round

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const choises = ["rock", "paper", "scissors"];
+let playerSelection = prompt("Chose your weapon!").toLowerCase();
+let computerSelection = choises[Math.floor(Math.random() * 3)];
 
-function computerPlay() {
+/*function computerPlay() {
     computerChoise = choises[Math.floor(Math.random() * 3)];
     return computerChoise;
+}*/
+
+function oneRound(playerSelection, computerSelection) {
+    if((playerSelection === "rock" && computerSelection === "paper") ||
+     (playerSelection === "paper" && computerSelection === "scissors") || 
+     (playerSelection === "scissors" && computerSelection === "rock")) {
+        return "You Lose!!";
+    } else if((playerSelection === "paper" && computerSelection === "rock") || 
+    (playerSelection === "scissors" && computerSelection === "paper") || 
+    (playerSelection === "rock" && computerSelection === "scissors")) {
+        return "You Win!!";
+    } else if(playerSelection === computerSelection) {
+        return "It's a Tie!!";
+    } else {
+        return "Wrong Choise!!";
+    }
 }
